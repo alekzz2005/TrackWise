f24d8e6d62dc4e8cfb572dbf3cc3af66c68ecb8d
alekzz2005 <binagatanalexander2005@gmail.com>
2025-10-31 17:35:45 +0800

    Sync local to repo

--- dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "dashboard"
-
-urlpatterns = [
-    path("dashboard/", views.dashboard_view, name="dashboard"),
-    path("add_inventory/", views.add_inventory_view, name="add_inventory"),
-]
--- dashboard/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-@login_required
-def dashboard_view(request):
-    return render(request, "dashboard/dashboard.html")
-
-def add_inventory_view(request):
-    return render (request, "dashboard/add_inventory.html")
